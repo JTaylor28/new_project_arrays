27be4c8c9de271098a7ae28c14a3f7c907ea8857
James Taylor <jrtaylor0614@gmail.com>
2022-11-02 18:55:29 -0600

    Implements array methods pop, shift, unshift, length with comments of method uses

--- arrays.rb
+++ arrays.rb
@@ -1,1 +1,22 @@
-array
+strings = ["jill","jeff","john","bill","beau"]
+puts strings
+integers = [1,2,3,4,] 
+puts integers
+floats = [1.1,2.2,3.3,4.4]
+puts floats
+booleans = [true,true,false,false]
+puts booleans
+
+# in this case, pop is removing "beau" from "strings" and returning it
+strings.pop 
+
+# in this case, shift is removing "1" from "integers" and returning it
+integers.shift
+
+# in this case, unshift is adding 0.0 to space 0 in "floats"
+# as well as shifting all all other elements forward by an index of 1
+floats.unshift (0.0)
+
+# in this case, length is stating the number of elements in "booleans"
+# which is 4
+booleans.length
